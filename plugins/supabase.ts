2753427351e1a8caabd25bb014e0a6aae46010b6
Mitch Chanza <mitch.chanza@gmail.com>
2023-01-08 17:04:28 +0200

    feat: update supabase plugin

--- plugins/supabase.ts
+++ plugins/supabase.ts
@@ -5,8 +5,12 @@ export default defineNuxtPlugin((nuxtApp) => {
     const config = useRuntimeConfig();
     console.log(config.public)
     const supabase = createClient(config.public.SUPABASE_URL, config.public.SUPABASE_API_KEY);
+    // @ts-ignore
+
 
     // allow us to inject, see composables
     // nuxtApp.vueApp.provide('supabase', supabase);
     nuxtApp.provide('supabase', supabase);
+
+
 });
